5aebf93a10a8d5ef0602f2f12c1526c9b74a4647
alexanderian76 <alexanderian76@gmail.com>
2025-12-03 17:25:00 +0300

    Minor

--- src/main/java/com/example/demo/controller/HomeRestController.java
+++ src/main/java/com/example/demo/controller/HomeRestController.java
@@ -3,12 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.dto.UserDto;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import org.jspecify.annotations.NonNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Stream;
 
 @RestController()
@@ -35,25 +35,25 @@ public class HomeRestController {
 
     // GET пользователь по ID
     @GetMapping("/{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+    public ResponseEntity<@NonNull UserDto> getUserById(@PathVariable Long id) {
         return _userService.getUserById(id)
                 .map(user -> ResponseEntity.ok(user.toDto()))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/search")
-    public ResponseEntity<UserDto> searchUser(@RequestParam(name = "name", required = true) String name) {
+    public ResponseEntity<@NonNull UserDto> searchUser(@RequestParam(name = "name", required = true) String name) {
         return ResponseEntity.ok(_userService.searchUser(name).toDto());
     }
 
     @GetMapping("/searchAll")
-    public ResponseEntity<Stream<UserDto>> searchUsers(@RequestParam(name = "name", required = true) String name) {
-        return ResponseEntity.ok(_userService.searchUsers(name).stream().map(u -> u.toDto()));
+    public ResponseEntity<@NonNull Stream<UserDto>> searchUsers(@RequestParam(name = "name", required = true) String name) {
+        return ResponseEntity.ok(_userService.searchUsers(name).stream().map(User::toDto));
     }
 
     // POST создание пользователя
     @GetMapping("/create/{id}")
-    public ResponseEntity<UserDto> createUser(@PathVariable Long id) {
+    public ResponseEntity<@NonNull UserDto> createUser(@PathVariable Long id) {
         User user = new User();
        // user.setAge(7);
         user.setCreatedAt(LocalDateTime.now());
